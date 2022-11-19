a6a14855347315e564918ca7edb2c592fd3194c3
Yaroslav <yarikmeisan@yandex.com>
2022-11-19 15:49:47 +0300

    Create Hz setup in wave

--- draw-graph.py
+++ draw-graph.py
@@ -1,28 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 
-# Get x values of the sine wave
-time = np.arange(0, 1, 0.01)
+freq = 1 # Frequency in Hz
+wy = 2 * np.pi * freq
+fs = 60 # point / sec
+maxTime = 3 # Seconds
+t = np.linspace(0, maxTime, fs * maxTime)
 
-# Amplitude of the sine wave is sine of a variable like time
-amplitude = np.cos(time*19.1)
-
-# Plot a sine wave using time and amplitude obtained for the sine wave
-plot.plot(time, amplitude)
-
-# Give a title for the sine wave plot
-plot.title('Cos wave')
-
-# Give x axis label for the sine wave plot
-plot.xlabel('Time')
-
-# Give y axis label for the sine wave plot
-plot.ylabel('Amplitude = cos(time)')
-
-plot.grid(True, which='both')
-
-plot.axhline(y=0, color='k')
-plot.show()
-
-# Display the sine wave
-plot.show()
+wave = np.sin(wy * t)
+plt.plot(t, wave)
+plt.show()
