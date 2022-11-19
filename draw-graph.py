8f8b99041170d85b556a5ae181ce3bc1d688a919
Yaroslav <yarikmeisan@yandex.com>
2022-11-19 17:16:21 +0300

    Set up plot size

--- draw-graph.py
+++ draw-graph.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+figure(figsize=(16, 6), dpi=80)
+
 
 waveType = input("Enter type of wave: cos or sin:  ")
 freq = int(input("Enter wave frequency:  "))
@@ -14,8 +18,6 @@ if waveType == "cos":
     wave = np.cos(pc * t)
 elif waveType == "sin":
     wave = np.sin(pc * t)
-
-
 
 plt.plot(t, wave)
 plt.show()
