208fa1c22d420ad2edbf9905c0cfa44ed097c0f9
Yaroslav <yarikmeisan@yandex.com>
2022-11-23 20:52:12 +0300

    Polar plot py

--- circle-plot.py
+++ circle-plot.py
@@ -1,20 +1,19 @@
 # Program to plot a Circle
 # using Parametric equation of a Circle
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-theta = np.linspace( 0 , 2 * np.pi , 150 )
+theta = np.linspace(0, 2 * np.pi, 150)
 
 radius = 0.4
 
-a = radius * np.cos( theta )
-b = radius * np.sin( theta )
+a = radius * np.cos(theta)
+b = radius * np.sin(theta)
 
-figure, axes = plt.subplots( 1 )
+figure, axes = plt.subplots(1)
 
-axes.plot( a, b )
-axes.set_aspect( 1 )
+axes.plot(a, b)
+axes.set_aspect(1)
 
-plt.title( 'Parametric Equation Circle' )
+plt.title('Parametric Equation Circle')
 plt.show()
